261eec434896a10478752164a3430bb73dd61d17
guytristramux <48714582+guytristramux@users.noreply.github.com>
2024-09-18 17:13:20 +0100

    updates to destination

--- app/routes/latest/destination.js
+++ app/routes/latest/destination.js
@@ -54,7 +54,7 @@ module.exports = function (router) {
     router.get('/' + version + section + '/routing-start', function (req, res)
     {
         // If Yes was selected, continue to next page
-        if (req.session.data['origin-to-or-from-own-premises-radios'] == "On to my premises/farm")
+        if (req.session.data['origin-to-or-from-own-premises-radios'] == "On the farm")
         {
             // Continue to the next pages where farmer is the origin
 
@@ -161,7 +161,7 @@ module.exports = function (router) {
             else
             {
                 // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('destination-farm-cph');
+                res.redirect('destination-tb-finishing-unit-cph');
             }
         }
 
@@ -280,11 +280,129 @@ module.exports = function (router) {
                 else
                 {
                     // This page name needs to be the next page the user gets to after successfully continuing
+                    res.redirect('reason-for-movement');
+                }
+            }
+        }
+    })
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////        CPH of destination Finishing Unit           ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + version + section + '/destination-tb-finishing-unit-cph-router', function (req, res)
+    {
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+        req.session.data['errortypetwo'] = "false";
+        req.session.data['errortypethree'] = "false";
+        req.session.data['errortypefour'] = "false";
+
+        // Validation check if field is blank
+        if (req.session.data['destination-tb-finishing-unit-cph-text-input'] == undefined || req.session.data['destination-tb-finishing-unit-cph-text-input'] == "")
+        {
+            // Trigger validation and relaunch the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('destination-farm-cph');
+        }
+
+        else if (req.session.data['destination-tb-finishing-unit-cph-text-input'].length > 13)
+        {
+            // Trigger validation and relaunch the page for over 15 characters
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypetwo'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('destination-farm-cph');
+        }
+
+        else if (req.session.data['destination-tb-finishing-unit-cph-text-input'].length < 9)
+        {
+            // Trigger validation and relaunch the page for under 5 characters
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypethree'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('destination-farm-cph');
+        }
+
+        else
+        {
+            // check no illegal charcters have been used
+            const acceptableCharacters =  " 0123456789/-";
+            let inputtext = req.session.data['destination-tb-finishing-unit-cph-text-input'];
+
+            let dissallowedCharacters = "";
+
+            // go through every character in the input and save  illegals ones
+            for (var i = 0; i < inputtext.length; i++)
+            {
+                let  singlecharacter = inputtext.charAt(i);
+
+                if( acceptableCharacters.includes( singlecharacter ) )
+                {
+                    // character is fine skip it
+                }
+                else
+                {
+                    // save this invalid character
+                    // if character is alread in tsring then don't add it
+                    if( dissallowedCharacters.includes( singlecharacter ) == false )
+                    {
+                        dissallowedCharacters = dissallowedCharacters.concat(singlecharacter);
+                    }
+                }
+            }
+
+            if(0 < dissallowedCharacters.length)
+            {
+                req.session.data['dissallowedcharacters'] = dissallowedCharacters;
+
+                // Trigger validation and relaunch the page for invalid characters
+                req.session.data['errorthispage'] = "true";
+                req.session.data['errortypefour'] = "true";
+
+                // This page name needs to match the page the user was just on
+                res.redirect('destination-farm-cph');
+            }
+            else
+            {
+                // everything with the input is fine so move on to next page
+
+                // If the user needs to go back to 'check your answers' then take them directly there
+                if (req.session.data['camefromcheckanswers'] == 'true')
+                {
+                    req.session.data['camefromcheckanswers'] = false;
+                    res.redirect('check-answers');
+                }
+                else
+                {
+                    // This page name needs to be the next page the user gets to after successfully continuing
                     res.redirect('check-answers');
                 }
             }
         }
     })
+
+
+
+
+
 
 
 
@@ -404,7 +522,7 @@ module.exports = function (router) {
             else
             {
                 // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('check-answers');
+                res.redirect('reason-for-movement');
             }
         }
 
@@ -421,7 +539,7 @@ module.exports = function (router) {
             else
             {
                 // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('check-answers');
+                res.redirect('reason-for-movement');
             }
         }
 
@@ -435,6 +553,290 @@ module.exports = function (router) {
             res.redirect('selection-of-own-premises');
         }
     })
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////            Reason for movement                     ////////////////
+    ////////////////            RADIO BUTTONS - MANDATORY               ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + version + section + '/reason-for-movement-router', function (req, res)
+    {
+        // Turn errors off by default
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+
+        // If Yes was selected, continue to next page
+        if (req.session.data['destination-reason-for-movement-radios'] == "Restocking")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('quantity-options');
+            }
+        }
+        else if (req.session.data['destination-reason-for-movement-radios'] == "Bull")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('any-additional-info');
+            }
+        }
+        else if (req.session.data['destination-reason-for-movement-radios'] == "Calf welfare")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('any-additional-info');
+            }
+        }
+        else if (req.session.data['destination-reason-for-movement-radios'] == "Welfare")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('any-additional-info');
+            }
+        }
+        else if (req.session.data['destination-reason-for-movement-radios'] == "Something else")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('any-additional-info');
+            }
+        }
+        else
+        {
+            // Trigger validation and reload the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('reason-for-movement');
+        }
+    })
+
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////             75 or more cattle                      ////////////////
+    ////////////////            RADIO BUTTONS - MANDATORY               ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + version + section + '/quantity-options-router', function (req, res)
+    {
+        // Turn errors off by default
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+
+        // If Yes was selected, continue to next page
+        if (req.session.data['destination-quantity-options-radios'] == "No")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('quantity-half-herd');
+            }
+        }
+        else if (req.session.data['destination-quantity-options-radios'] == "Yes")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('check-answers');
+            }
+        }
+        else if (req.session.data['destination-quantity-options-radios'] == "I don't know")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('check-answers');
+            }
+        }
+        else
+        {
+            // Trigger validation and reload the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('quantity-options');
+        }
+    })
+
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////         Half the herd size or not                  ////////////////
+    ////////////////            RADIO BUTTONS - MANDATORY               ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + version + section + '/quantity-half-herd-router', function (req, res)
+    {
+        // Turn errors off by default
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+
+        // If Yes was selected, continue to next page
+        if (req.session.data['destination-quantity-half-herd-radios'] == "No")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('check-answers');
+            }
+        }
+        else if (req.session.data['destination-quantity-half-herd-radios'] == "Yes")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('check-answers');
+            }
+        }
+        else if (req.session.data['destination-quantity-half-herd-radios'] == "I don't know")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('check-answers');
+            }
+        }
+        else
+        {
+            // Trigger validation and reload the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('quantity-half-herd');
+        }
+    })
+
+
 
 
 
