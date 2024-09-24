3f5f51ff9054fc169513b2e8d90f9df21b0dc5ab
guytristramux <48714582+guytristramux@users.noreply.github.com>
2024-09-24 14:58:02 +0100

    most of the way through first draft of biosecurity questions

--- app/routes/latest/biosecurity.js
+++ app/routes/latest/biosecurity.js
@@ -44,6 +44,32 @@ module.exports = function (router) {
     ////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////
     ////////////////                                                    ////////////////
+    ////////////////       Initial routing based on cattle type         ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    router.get('/' + section + '/begin-biosecurity-details-section', function (req, res)
+    {
+        // If the movement is a bull or welfare then don't ask biosecurity questions
+        if (req.session.data['destination-reason-for-movement-radios'] == 'Bull' ||
+            req.session.data['destination-reason-for-movement-radios'] == 'Welfare')
+        {
+            res.redirect('people-disinfection');
+        }
+        else
+        {
+            // This page name needs to be the next page the user gets to after successfully continuing
+            res.redirect('kept-separately');
+        }
+    })
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
     ////////////////       Will incoming cattle be kept separately?     ////////////////
     ////////////////                                                    ////////////////
     ////////////////////////////////////////////////////////////////////////////////////
@@ -87,7 +113,7 @@ module.exports = function (router) {
             else
             {
                 // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('upload-plan');
+                res.redirect('people-disinfection');
             }
         }
         else
@@ -151,7 +177,7 @@ module.exports = function (router) {
             else
             {
                 // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('upload-plan');
+                res.redirect('roads-and-tracks');
             }
         }
         else
@@ -211,11 +237,399 @@ module.exports = function (router) {
             else
             {
                 // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('upload-plan');
+                res.redirect('manure-and-slurry');
             }
         }
 
     })
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       Manure and slurry in the past 60 days        ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       YES AND NO - RADIO BUTTONS - MANDATORY       ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + section + '/manure-and-slurry-router', function (req, res)
+    {
+        // Turn errors off by default
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+
+        // If Yes was selected, continue to next page
+        if (req.session.data['biosecurity-manure-and-slurry-radios-yes-no'] == "Yes")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('grazing-field-how-separated');
+            }
+        }
+        else if (req.session.data['biosecurity-manure-and-slurry-radios-yes-no'] == "No")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('grazing-field-how-separated');
+            }
+        }
+        else
+        {
+            // Trigger validation and reload the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('manure-and-slurry');
+        }
+    })
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       Initial routing based on cattle type         ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    router.get('/' + section + '/grazing-field-how-separated-router', function (req, res)
+    {
+        // Always proceed to the next questions on shared tracks
+
+        res.redirect('roads-and-tracks');
+        // Possibly add empty field error in future
+    })
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////            Any shared roads and tracks             ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       YES AND NO - RADIO BUTTONS - MANDATORY       ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + section + '/roads-and-tracks-router', function (req, res)
+    {
+        // Turn errors off by default
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+
+        // If Yes was selected, continue to next page
+        if (req.session.data['biosecurity-roads-and-tracks-radios-yes-no'] == "Yes")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('road-and-track-minimise-contamination');
+            }
+        }
+        else if (req.session.data['biosecurity-roads-and-tracks-radios-yes-no'] == "No")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('PLACEHOLDER_SHARED_BUILDINGS');
+            }
+        }
+        else
+        {
+            // Trigger validation and reload the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('roads-and-tracks');
+        }
+    })
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////              How will tracks be kept clean         ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////                        TEXT AREA                   ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    router.get('/' + section + '/road-and-track-minimise-contamination-router', function (req, res)
+    {
+        // Always proceed to the next questions on shared tracks
+
+        res.redirect('buildings-any-shared');
+        // Possibly add empty field error in future
+    })
+
+
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////          Any shared buildings with the herd        ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       YES AND NO - RADIO BUTTONS - MANDATORY       ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + section + '/buildings-any-shared-router', function (req, res)
+    {
+        // Turn errors off by default
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+
+        // If Yes was selected, continue to next page
+        if (req.session.data['biosecurity-buildings-any-shared-radios-yes-no'] == "Yes")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('buildings-how-minimise-contamination');
+            }
+        }
+        else if (req.session.data['biosecurity-buildings-any-shared-radios-yes-no'] == "No")
+        {
+            // Continue to the next page
+
+            // If the user needs to go back to 'check your answers' then take them directly there
+            if (req.session.data['camefromcheckanswers'] == 'true')
+            {
+                req.session.data['camefromcheckanswers'] = false;
+                res.redirect('check-answers');
+            }
+            else
+            {
+                // This page name needs to be the next page the user gets to after successfully continuing
+                res.redirect('people-disinfection');
+            }
+        }
+        else
+        {
+            // Trigger validation and reload the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('buildings-any-shared');
+        }
+    })
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       How will shared buildings be kept clean      ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////                        TEXT AREA                   ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    router.get('/' + section + '/buildings-how-minimise-contamination-router', function (req, res)
+    {
+        // Always proceed to the next questions on shared tracks
+
+        res.redirect('people-disinfection');
+        // Possibly add empty field error in future
+    })
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////       How people avoid contamination               ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////                        TEXT AREA                   ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    router.get('/' + section + '/people-disinfection-router', function (req, res)
+    {
+        // Always proceed to the next questions on shared tracks
+
+        res.redirect('disinfectant');
+        // Possibly add empty field error in future
+    })
+
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////                                                    ////////////////
+    ////////////////        What disinfectant are is being used         ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////               TEXT ENTRY - MANDATORY               ////////////////
+    ////////////////                                                    ////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+    // NOT COMPLEX PAGE
+    router.post('/' + section + '/disinfectant-router', function (req, res)
+    {
+        req.session.data['errorthispage'] = "false";
+        req.session.data['errortypeone'] = "false";
+        req.session.data['errortypetwo'] = "false";
+        req.session.data['errortypethree'] = "false";
+        req.session.data['errortypefour'] = "false";
+
+        // Validation check if field is blank
+        if (req.session.data['biosecurity-disinfectant-text-input'] == undefined || req.session.data['biosecurity-disinfectant-text-input'] == "")
+        {
+            // Trigger validation and relaunch the page
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypeone'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('disinfectant');
+        }
+
+
+        else if (req.session.data['biosecurity-disinfectant-text-input'].length < 4)
+        {
+            // Trigger validation and relaunch the page for under 5 characters
+            req.session.data['errorthispage'] = "true";
+            req.session.data['errortypethree'] = "true";
+
+            // This page name needs to match the page the user was just on
+            res.redirect('disinfectant');
+        }
+
+
+
+
+        // everything with the input is fine so move on to next page
+
+        // If the user needs to go back to 'check your answers' then take them directly there
+        else if (req.session.data['camefromcheckanswers'] == 'true')
+        {
+            req.session.data['camefromcheckanswers'] = false;
+            res.redirect('check-answers');
+        }
+        else
+        {
+            // This page name needs to be the next page the user gets to after successfully continuing
+            res.redirect('disinfectant-percentage');
+        }
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////
--- app/routes/latest/destination.js
+++ app/routes/latest/destination.js
@@ -578,7 +578,7 @@ module.exports = function (router) {
         req.session.data['errortypeone'] = "false";
 
         // If Yes was selected, continue to next page
-        if (req.session.data['destination-reason-for-movement-radios'] == "Restocking")
+        if (req.session.data['destination-reason-for-movement-radios'] == "Routine restocking")
         {
             // Continue to the next page
 
@@ -595,22 +595,6 @@ module.exports = function (router) {
             }
         }
         else if (req.session.data['destination-reason-for-movement-radios'] == "Bull")
-        {
-            // Continue to the next page
-
-            // If the user needs to go back to 'check your answers' then take them directly there
-            if (req.session.data['camefromcheckanswers'] == 'true')
-            {
-                req.session.data['camefromcheckanswers'] = false;
-                res.redirect('check-answers');
-            }
-            else
-            {
-                // This page name needs to be the next page the user gets to after successfully continuing
-                res.redirect('any-additional-info');
-            }
-        }
-        else if (req.session.data['destination-reason-for-movement-radios'] == "Calf welfare")
         {
             // Continue to the next page
 
