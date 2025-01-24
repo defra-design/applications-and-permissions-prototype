6a8268e8cdf82dd5c63453cb598d0271231a4f77
guytristramux <48714582+guytristramux@users.noreply.github.com>
2025-01-24 09:15:08 +0000

    split into releases and versions for review

--- app/routes.js
+++ app/routes.js
@@ -19,6 +19,7 @@ require('./routes/v1_0_0/version.js');
 require('./routes/v1_1_0/version.js');
 require('./routes/v1_2_0/version.js');
 require('./routes/v2_0_0/version.js');
+require('./routes/v2_1_0/version.js');
 require('./routes/v3_0_0/version.js');
 require('./routes/latest/version.js');
 
