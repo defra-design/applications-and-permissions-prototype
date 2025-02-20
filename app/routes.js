af442b186ff1dd7614dd5731963ac10463498ef8
guytristramux <48714582+guytristramux@users.noreply.github.com>
2025-02-20 14:44:52 +0000

    more removing

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,6 @@ require('./routes/v1_1_0/version.js');
 require('./routes/v1_2_0/version.js');
 require('./routes/v2_0_0/version.js');
 require('./routes/v2_1_0/version.js');
-require('./routes/v3_0_0/version.js');
 require('./routes/latest/version.js');
 
 
