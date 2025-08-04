38f2ff9aadb3d55220db34dc115e40aee8c6942e
guytristramux <48714582+guytristramux@users.noreply.github.com>
2025-08-04 10:12:00 +0100

    first lot of pages for user facing exotics - routing to be sorted next

---
+++ app/routes/v3_0_0/exotics-about-the-movement.js
@@ -0,0 +1,21 @@
+const {log} = require("govuk-prototype-kit/migrator/logger");
+module.exports = function (router) {
+
+
+
+
+    /*
+        Setting a section in each routes.js file
+        This allows you to have different routes.js files for each section of your prototype
+        e.g  registration.js   and   search.js
+        This avoids having one huge hard to manage routes.js
+     */
+    let section = "/exotics/about-the-movement/";
+
+
+
+
+
+
+
+}
