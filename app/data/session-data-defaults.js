21b0b60779fcdc88a8e3c95044274e89b504f35b
guytristramux <48714582+guytristramux@users.noreply.github.com>
2026-01-12 14:31:48 +0000

    case management updates

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,6 +3,8 @@ module.exports = {
   // Insert values here
 
     'bluetongue' : 'false',
-    'customer-identity' : 'true',
-    'defra-account-registration' : 'true'
+    'customer-identity' : 'false',
+    'defra-account-registration' : 'false',
+
+    'staff-user-name' : 'Alex Smith'
 }
