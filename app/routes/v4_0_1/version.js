6167de252867eb7357b0af2d44827e4f4e6789b5
guytristramux <48714582+guytristramux@users.noreply.github.com>
2026-02-03 13:37:08 +0000

    Version 4.0.1

---
+++ app/routes/v4_0_1/version.js
@@ -0,0 +1,31 @@
+const govukPrototypeKit = require('govuk-prototype-kit');
+
+const version = 'v4_0_1';
+
+const router = govukPrototypeKit.requests.setupRouter('/' + version);
+
+// Import subfiles
+require('./what.js')(router);
+require('./origin.js')(router);
+require('./destination.js')(router);
+require('./tests.js')(router);
+require('./biosecurity.js')(router);
+require('./biosecurity-map.js')(router);
+require('./reactors.js')(router);
+require('./identification.js')(router);
+require('./contact-and-updates.js')(router);
+require('./submit.js')(router);
+require('./gov-gateway.js')(router);
+require('./all-diseases-configurator.js')(router);
+require('./exotics-about-the-movement.js')(router);
+require('./exotics-movement-origin.js')(router);
+require('./exotics-fmd.js')(router);
+require('./case-management.js')(router);
+
+
+
+
+
+
+
+
