4b2577a59afb609bc252955bc7da991f70e8cf96
guytristramux <48714582+guytristramux@users.noreply.github.com>
2026-04-02 13:56:00 +0100

    update for handover

--- app/routes.js
+++ app/routes.js
@@ -9,8 +9,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 // Including other routing javascript
-// This single line tells this 'routes.js' file to include the routing from the 'templates.js' file
-require('./routes/templates.js');
+
 require('./routes/v0_1_0/version.js');
 require('./routes/v0_2_0/version.js');
 require('./routes/v0_4_0/version.js');
